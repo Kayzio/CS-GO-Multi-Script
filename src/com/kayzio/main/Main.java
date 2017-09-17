8fa6742528f0a81de17a0116a7cd9f16d9056c75
Daniel Ilinykh <kayziogroups@gmail.com>
2017-09-17 03:07:48 -0400

    Finally figured out how java separates .class and .java files :P

---
+++ src/com/kayzio/main/Main.java
@@ -0,0 +1,17 @@
+package com.kayzio.main;
+
+class Main {
+
+	public Main(){
+		printFag();
+	}
+
+	void printFag(){
+		System.out.println("Fag");
+	}
+
+	public static void main(String[] args){
+		Main main = new Main();
+	}
+
+}
